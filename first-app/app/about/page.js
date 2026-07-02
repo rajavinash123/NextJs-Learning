ca408aa670c70bdbc19f6464b351c71adc51b232
rajavinash123 <avinashkumar808482@gmail.com>
2026-07-02 21:45:20 +0530

    "day one to learn next js"

---
+++ first-app/app/about/page.js
@@ -0,0 +1,17 @@
+const Page = () => {
+  return (
+    <div className="p-10">
+      <h1 className="text-3xl font-bold mb-4">
+        Problem Solving by Next.js
+      </h1>
+
+      <ul className="list-disc pl-6 space-y-2">
+        <li>Problem solving with Next.js</li>
+        <li>Easy routing in Next.js</li>
+        <li>File-based routing</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Page;
